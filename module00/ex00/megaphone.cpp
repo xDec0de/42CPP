289c64fa26dc5b2fb7a0fa7879fd61e1365670e2
danimart <danimart@student.42madrid.com>
2023-11-16 11:51:48 +0100

    Makefile and hello world :D

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 11:35:36 by danimart          #+#    #+#             */
+/*   Updated: 2023/11/16 11:51:04 by danimart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	(void) argc;
+	(void) argv;
+	std::cout << "Hello world" << std::endl;
+	return 0;
+}
