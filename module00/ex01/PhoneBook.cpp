0c837d5145fbbb4794b4689712074883381cb7c4
danimart <danimart@student.42madrid.com>
2023-11-16 16:11:35 +0100

    User input test for ADD command

--- module00/ex01/PhoneBook.cpp
+++ module00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:13:46 by danimart          #+#    #+#             */
-/*   Updated: 2023/11/16 16:03:19 by danimart         ###   ########.fr       */
+/*   Updated: 2023/11/16 16:05:14 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,5 @@ Contact PhoneBook::addContact(std::string first_name, std::string last_name, std
 	contact.setFirstName(first_name);
 	contact.setLastName(last_name);
 	contact.setNickname(nickname);
-	std::cout << "First name: " + contact.getFirstName() << std::endl;
-	std::cout << "Last name: " + contact.getLastName() << std::endl;
-	std::cout << "Nickname: " + contact.getNickname() << std::endl;
 	return (contact);
 }
--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -6,15 +6,31 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:35:04 by danimart          #+#    #+#             */
-/*   Updated: 2023/11/16 16:03:54 by danimart         ###   ########.fr       */
+/*   Updated: 2023/11/16 16:11:09 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
+std::string process_add(PhoneBook phonebook)
+{
+	std::string first_name;
+	std::cout << "Input first name:" << std::endl;
+	std::cin >> first_name;
+	std::string last_name;
+	std::cout << "Input last name:" << std::endl;
+	std::cin >> last_name;
+	std::cout << "Input nickname:" << std::endl;
+	std::string nickname;
+	std::cin >> nickname;
+	phonebook.addContact(first_name, last_name, nickname);
+	return ("ADD");
+}
+
 std::string process_cmd(std::string cmd, PhoneBook phonebook)
 {
-	phonebook.addContact("First", "Second", "Third");
+	if (cmd == "ADD")
+		return (process_add(phonebook));
 	return (cmd);
 }
 
