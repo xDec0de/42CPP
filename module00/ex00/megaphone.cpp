33a46ba0a04ba19186c3fb0735dc8631f1bb0006
danimart <danimart@student.42madrid.com>
2023-11-16 12:51:07 +0100

    Megaphone: Remove consecutive spaces

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,18 +6,20 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 11:35:36 by danimart          #+#    #+#             */
-/*   Updated: 2023/11/16 12:38:02 by danimart         ###   ########.fr       */
+/*   Updated: 2023/11/16 12:50:33 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-std::string ft_toupper(std::string str)
+std::string to_megaphone(std::string str)
 {
+	std::string upper = "";
 	int len = str.length();
 	for (int i = 0; i < len; i++)
-		str[i] = toupper(str[i]);
-	return (str);
+		if (i == 0 || str[i] != ' ' || (str[i] == ' ' && str[i - 1] != ' '))
+			upper += toupper(str[i]);
+	return (upper);
 }
 
 std::string get_input(int argc, char const *argv[])
@@ -27,7 +29,7 @@ std::string get_input(int argc, char const *argv[])
 	std::string input = argv[1];
 	for (int i = 2; i < argc; i++)
 		input = input + ' ' + argv[i];
-	return (ft_toupper(input));
+	return (to_megaphone(input));
 }
 
 int main(int argc, char const *argv[])
