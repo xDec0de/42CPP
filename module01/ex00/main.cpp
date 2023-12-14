d10cd9d359a948bfb282d151be6473524c4069a3
danimart <danimart@student.42madrid.com>
2023-12-14 17:29:10 +0100

    m01/ex00: Added main

--- module01/ex00/main.cpp
+++ module01/ex00/main.cpp
@@ -6,7 +6,20 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 16:42:02 by danimart          #+#    #+#             */
-/*   Updated: 2023/12/14 16:42:03 by danimart         ###   ########.fr       */
+/*   Updated: 2023/12/14 17:28:27 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+int main(void)
+{
+	Zombie *z = newZombie("OriginalZombieName");
+	z->announce();
+	randomChump("Chump");
+	delete z;
+	return (0);
+}
