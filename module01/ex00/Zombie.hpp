3c08cb7e70fc30e0b79829ca3b3cd35a6483735a
danimart <danimart@student.42madrid.com>
2023-12-14 17:15:07 +0100

    m01/ex00: Privatize default Zombie constructor

--- module01/ex00/Zombie.hpp
+++ module01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 16:41:59 by danimart          #+#    #+#             */
-/*   Updated: 2023/12/14 17:08:03 by danimart         ###   ########.fr       */
+/*   Updated: 2023/12/14 17:14:29 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 class Zombie
 {
 	private:
+		Zombie(void);
 		std::string name;
 	public:
 		Zombie(std::string name);
