8dee479c92b4416accd90c8b03cf5c27ec71541a
danimart <danimart@student.42madrid.com>
2023-12-14 17:12:25 +0100

    m01/ex00: Added basic Zombie implementation

--- module01/ex00/Zombie.cpp
+++ module01/ex00/Zombie.cpp
@@ -6,7 +6,21 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 16:41:57 by danimart          #+#    #+#             */
-/*   Updated: 2023/12/14 16:41:57 by danimart         ###   ########.fr       */
+/*   Updated: 2023/12/14 17:11:16 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie::Zombie(std::string name) {
+	this->name = name;
+}
+
+Zombie::~Zombie(void) {
+	std::cout << name + " died.";
+}
+
+void Zombie::announce(void) {
+	std::cout << name + ": BraiiiiiiinnnzzzZ...";
+}
--- module01/ex00/Zombie.hpp
+++ module01/ex00/Zombie.hpp
@@ -6,7 +6,23 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 16:41:59 by danimart          #+#    #+#             */
-/*   Updated: 2023/12/14 16:42:00 by danimart         ###   ########.fr       */
+/*   Updated: 2023/12/14 17:08:03 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+
+#include <string>
+
+class Zombie
+{
+	private:
+		std::string name;
+	public:
+		Zombie(std::string name);
+		~Zombie(void);
+		void announce(void);
+};
+
+#endif
