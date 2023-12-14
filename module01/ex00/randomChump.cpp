89635ec82e372e68fe0be500f1c4f5ef14936395
danimart <danimart@student.42madrid.com>
2023-12-14 17:23:39 +0100

    m01/ex00: Implement randomChump

--- module01/ex00/randomChump.cpp
+++ module01/ex00/randomChump.cpp
@@ -6,7 +6,14 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 16:44:03 by danimart          #+#    #+#             */
-/*   Updated: 2023/12/14 16:44:03 by danimart         ###   ########.fr       */
+/*   Updated: 2023/12/14 17:21:43 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+void randomChump(std::string name) {
+	Zombie *zombie = new Zombie(name);
+	zombie->announce();
+	delete zombie;
+}
