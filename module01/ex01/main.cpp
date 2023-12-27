e2077f08a744fb1ceb250d468d1323a7ca669bb9
danimart <danimart@student.42madrid.com>
2023-12-27 22:23:22 +0100

    m01/ex02: Finish exercise (Too easy to be true)

--- module01/ex01/main.cpp
+++ module01/ex01/main.cpp
@@ -6,11 +6,25 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 21:55:31 by danimart          #+#    #+#             */
-/*   Updated: 2023/12/27 22:17:37 by danimart         ###   ########.fr       */
+/*   Updated: 2023/12/27 22:21:13 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
 int main(void)
 {
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "The memory address of string is: " << &string << std::endl;
+	std::cout << "The memory address held by stringPTR is: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF is: " << &stringREF << std::endl;
+
+	std::cout << "The value of string is: " << string << std::endl;
+	std::cout << "The value pointed to by stringPTR is: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF is: " << stringREF << std::endl;
+
 	return 0;
 }
