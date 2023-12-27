56cd0cb51dd2d8442bb66d08b815731a33b7a354
danimart <danimart@student.42madrid.com>
2023-12-27 23:09:03 +0100

    m01/ex01: Added more tests

--- module01/ex01/main.cpp
+++ module01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 22:39:30 by danimart          #+#    #+#             */
-/*   Updated: 2023/12/27 22:45:41 by danimart         ###   ########.fr       */
+/*   Updated: 2023/12/27 23:08:32 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ Zombie* zombieHorde(const int &N, const std::string &name);
 
 int main(void) {
 	Zombie *horde = zombieHorde(5, "Horde");
+	std::cout << "Announcing a horde of five zombies..." << std::endl;
 	for (int i = 0; i < 5; i++)
 		horde[i].announce();
+	std::cout << "Empty horde: " << zombieHorde(0, "Empty horde") << std::endl;
+	std::cout << "Negative size horde: " << zombieHorde(-42, "Empty horde") << std::endl;
 	return 0;
 }
--- module01/ex01/zombieHorde.cpp
+++ module01/ex01/zombieHorde.cpp
@@ -6,14 +6,14 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 22:41:40 by danimart          #+#    #+#             */
-/*   Updated: 2023/12/27 22:53:55 by danimart         ###   ########.fr       */
+/*   Updated: 2023/12/27 23:03:52 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie* zombieHorde(const int &N, const std::string &name) {
-	if (N <= 0)
+	if (N < 0)
 		return NULL;
 	Zombie *horde = new Zombie[N];
 	for (int i = 0; i < N; i++)
