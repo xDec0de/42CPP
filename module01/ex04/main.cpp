6915132cbe240a7c7ba2098db2dda62a74dad5c6
danimart <danimart@student.42madrid.com>
2024-01-10 12:37:54 +0100

    m01/ex04: Open file & handle file open error

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,12 +6,13 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:24:32 by danimart          #+#    #+#             */
-/*   Updated: 2023/12/28 18:37:54 by danimart         ###   ########.fr       */
+/*   Updated: 2024/01/10 12:36:19 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sed.hpp"
 #include <iostream>
+#include <fstream>
 
 int prog_exit(std::string msg, int code) {
 	std::cout << msg << std::endl;
@@ -20,8 +21,13 @@ int prog_exit(std::string msg, int code) {
 
 int main(int argc, char const *argv[])
 {
-	(void) argv;
 	if (argc != 4)
 		prog_exit(ARGC_ERR, 1);
+	std::ifstream file(argv[1], std::ifstream::in);
+	if (!file.is_open())
+		prog_exit(OPEN_ERR, 2);
+	std::string to_find = argv[2];
+	std::string replacement = argv[3];
+	file.close();
 	return 0;
 }
--- module01/ex04/sed.hpp
+++ module01/ex04/sed.hpp
@@ -6,18 +6,14 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:34:14 by danimart          #+#    #+#             */
-/*   Updated: 2023/12/28 18:50:08 by danimart         ###   ########.fr       */
+/*   Updated: 2024/01/10 12:31:13 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SED_H
 # define SED_H
 
-# define BRED "\x1B[1;31m"
-# define RED "\x1B[0;31m"
-# define GRAY "\x1B[0;30m"
-# define RESET "\x1B[0m"
-
-# define ARGC_ERR BRED"Usage"GRAY": ./"RED"sed "GRAY"<"RED"file name"GRAY"> <"RED"text to replace"GRAY"> <"RED"replacement"GRAY">"RESET
+# define ARGC_ERR "Usage: ./sed <file name> <text to replace> <replacement>"
+# define OPEN_ERR "Error: Could not open file."
 
 #endif
