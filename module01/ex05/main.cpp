90cce51d90eb0bdac287b3aaca20df01ce9091fa
danimart <danimart@student.42madrid.com>
2024-01-10 17:29:44 +0100

    m01/ex05: Implement functions

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -6,10 +6,18 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:18:18 by danimart          #+#    #+#             */
-/*   Updated: 2024/01/10 16:18:38 by danimart         ###   ########.fr       */
+/*   Updated: 2024/01/10 16:47:31 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
 int main(void) {
+	Harl harl;
+	harl.complain("???");
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
 	return 0;
 }
