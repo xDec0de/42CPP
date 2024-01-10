6249f5cf83f5e847c0bf8795413ce44ef999ddf7
danimart <danimart@student.42madrid.com>
2024-01-10 16:21:16 +0100

    m01/ex05: Exercise setup

---
+++ module01/ex05/Harl.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 16:16:02 by danimart          #+#    #+#             */
+/*   Updated: 2024/01/10 16:17:34 by danimart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void Harl::debug(void) {
+	
+}
+
+void Harl::info(void) {
+
+}
+
+void Harl::warning(void) {
+
+}
+
+void Harl::error(void) {
+
+}
+
+void Harl::complain(const std::string &level) {
+
+}
---
+++ module01/ex05/Harl.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 16:14:19 by danimart          #+#    #+#             */
+/*   Updated: 2024/01/10 16:19:39 by danimart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_H
+# define HARL_H
+
+#include <string>
+
+class Harl
+{
+	public:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+		void complain(const std::string &level);
+};
+
+#endif
