1beb1a57fc03745d7919b0819f95ed4ee02d769e
danimart <danimart@student.42madrid.com>
2024-01-10 19:04:37 +0100

    m01/ex06: Program now accepts input

--- module01/ex06/main.cpp
+++ module01/ex06/main.cpp
@@ -6,14 +6,18 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 18:40:45 by danimart          #+#    #+#             */
-/*   Updated: 2024/01/10 19:00:45 by danimart         ###   ########.fr       */
+/*   Updated: 2024/01/10 19:03:44 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main(void) {
+int main(int argc, char const *argv[])
+{
 	Harl harl;
-	harl.complain("DEBUG");
+	std::string level = "";
+	if (argc >= 2)
+		level = argv[1];
+	harl.complain(level);
 	return 0;
 }
