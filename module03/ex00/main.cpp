c058afaf9b8cf7d9e4833455e79a916a2bdcc686
danimart <danimart@student.42madrid.com>
2024-02-20 14:37:41 +0100

    m03/ex00: Added better tests

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -6,23 +6,49 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 12:23:16 by danimart          #+#    #+#             */
-/*   Updated: 2024/02/20 12:57:46 by danimart         ###   ########.fr       */
+/*   Updated: 2024/02/20 14:33:05 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#include <iostream>
 
 int main() {
-	ClapTrap clapTrap("Test");
-	clapTrap.beRepaired(10);
-	clapTrap.beRepaired(1);
-	clapTrap.beRepaired(0);
-	clapTrap.takeDamage(10);
-	clapTrap.takeDamage(1);
-	clapTrap.takeDamage(0);
-	clapTrap.attack("Another ClapTrap");
-	clapTrap.takeDamage(500);
-	clapTrap.beRepaired(500);
-	clapTrap.attack("Should not be able to attack.");
+	// Default constructor test //
+	std::cout << "\nDefault constructor:" << std::endl;
+	ClapTrap def = ClapTrap();
+	def.attack("Another ClapTrap");
+	def.beRepaired(32);
+	def.takeDamage(32);
+
+	// String parameter constructor tests //
+	std::cout << "\nString parameter constructors:" << std::endl;
+	ClapTrap damage("Damage");
+	ClapTrap energy("Energy");
+	ClapTrap zero("Zero");
+
+	// Energy tests //
+	std::cout << "\nExecuting energy tests:" << std::endl;
+	for (int i = 0; i < 10; i++)
+		energy.attack("Another ClapTrap");
+	energy.attack("FAIL");
+	energy.takeDamage(1);
+
+	// Lethal damage tests //
+	std::cout << "\nExecuting lethal damage tests:" << std::endl;
+	damage.beRepaired(10);
+	damage.takeDamage(10);
+	damage.takeDamage(1000);
+	damage.attack("Should not be able to attack.");
+	damage.takeDamage(1);
+	damage.beRepaired(1);
+
+	// Zero amount tests //
+	std::cout << "\nExecuting zero amount tests:" << std::endl;
+	zero.takeDamage(0);
+	zero.beRepaired(0);
+
+	// Destructors //
+	std::cout << "\nDestructors:" << std::endl;
 	return 0;
 }
