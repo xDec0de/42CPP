63fc505daca6a231ecdfce5348c2baab4fe0f44b
danimart <danimart@student.42madrid.com>
2024-02-21 21:42:41 +0100

    m03/ex02: Added test for every ClapTrap type

--- module03/ex02/main.cpp
+++ module03/ex02/main.cpp
@@ -6,27 +6,40 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 12:23:16 by danimart          #+#    #+#             */
-/*   Updated: 2024/02/21 21:19:59 by danimart         ###   ########.fr       */
+/*   Updated: 2024/02/21 21:41:14 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 #include <iostream>
 
-int main() {
+void clapTests() {
+	// ClapTrap stat tests //
+	std::cout << "\nExecuting ClapTrap stats tests:" << std::endl;
+	ClapTrap clap("ClapTrap");
+	clap.attack("DamageZero");
+	clap.beRepaired(10);
+	clap.takeDamage(20);
+
+	// Destructors //
+	std::cout << "\nDestructors:" << std::endl;
+}
+
+void scavTests() {
 	// Default constructor test //
-	std::cout << "\nDefault constructor:" << std::endl;
+	std::cout << "\nDefault constructor (ScavTrap):" << std::endl;
 	ScavTrap def = ScavTrap();
 
 	// Basic tests //
-	std::cout << "\nBasic tests:" << std::endl;
+	std::cout << "\nBasic tests (ScavTrap):" << std::endl;
 	def.attack("ClapTrap");
 	def.takeDamage(58);
 	def.beRepaired(58);
 	def.guardGate();
 
 	// String parameter constructor tests //
-	std::cout << "\nString parameter constructors:" << std::endl;
+	std::cout << "\nString parameter constructors (ScavTrap):" << std::endl;
 	ScavTrap damage("Damage");
 	ScavTrap energy("Energy");
 	ScavTrap zero("Zero");
@@ -37,20 +50,13 @@ int main() {
 	copyCons.attack("NameShouldBe:unknown");
 
 	// Copy assignement operator tests //
-	std::cout << "\nExecuting copy assignement operator tests:" << std::endl;
+	std::cout << "\nExecuting copy assignement operator tests (ScavTrap):" << std::endl;
 	ScavTrap copyOp("CopyOperator");
 	def = copyOp;
 	def.attack("NameShouldBe:CopyOperator");
 
-	// ClapTrap stat tests //
-	std::cout << "\nExecuting ClapTrap stats tests:" << std::endl;
-	ClapTrap clap("ClapTrap");
-	clap.attack("DamageZero");
-	clap.beRepaired(10);
-	clap.takeDamage(20);
-
 	// Energy tests //
-	std::cout << "\nExecuting energy tests:" << std::endl;
+	std::cout << "\nExecuting energy tests (ScavTrap):" << std::endl;
 	for (int i = 0; i < 50; i++)
 		energy.attack("ClapTrap");
 	energy.attack("FAIL");
@@ -59,7 +65,7 @@ int main() {
 	energy.guardGate();
 
 	// Lethal damage tests //
-	std::cout << "\nExecuting lethal damage tests:" << std::endl;
+	std::cout << "\nExecuting lethal damage tests (ScavTrap):" << std::endl;
 	damage.beRepaired(10);
 	damage.takeDamage(10);
 	damage.takeDamage(1000);
@@ -69,11 +75,74 @@ int main() {
 	damage.guardGate();
 
 	// Zero amount tests //
-	std::cout << "\nExecuting zero amount tests:" << std::endl;
+	std::cout << "\nExecuting zero amount tests (ScavTrap):" << std::endl;
 	zero.takeDamage(0);
 	zero.beRepaired(0);
 
 	// Destructors //
 	std::cout << "\nDestructors:" << std::endl;
+}
+
+void fragTests() {
+	// Default constructor test //
+	std::cout << "\nDefault constructor (FragTrap):" << std::endl;
+	FragTrap def = FragTrap();
+
+	// Basic tests //
+	std::cout << "\nBasic tests (FragTrap):" << std::endl;
+	def.attack("ClapTrap");
+	def.takeDamage(58);
+	def.beRepaired(58);
+	def.highFivesGuys();
+
+	// String parameter constructor tests //
+	std::cout << "\nString parameter constructors (FragTrap):" << std::endl;
+	FragTrap damage("Damage");
+	FragTrap energy("Energy");
+	FragTrap zero("Zero");
+
+	// Copy constructor tests //
+	std::cout << "\nExecuting copy constructor tests:" << std::endl;
+	FragTrap copyCons(def);
+	copyCons.attack("NameShouldBe:unknown");
+
+	// Copy assignement operator tests //
+	std::cout << "\nExecuting copy assignement operator tests (FragTrap):" << std::endl;
+	FragTrap copyOp("CopyOperator");
+	def = copyOp;
+	def.attack("NameShouldBe:CopyOperator");
+
+	// Energy tests //
+	std::cout << "\nExecuting energy tests (FragTrap):" << std::endl;
+	for (int i = 0; i < 100; i++)
+		energy.attack("ClapTrap");
+	energy.attack("FAIL");
+	energy.takeDamage(1);
+	energy.beRepaired(1);
+	energy.highFivesGuys();
+
+	// Lethal damage tests //
+	std::cout << "\nExecuting lethal damage tests (FragTrap):" << std::endl;
+	damage.beRepaired(10);
+	damage.takeDamage(10);
+	damage.takeDamage(1000);
+	damage.attack("Should not be able to attack.");
+	damage.takeDamage(1);
+	damage.beRepaired(1);
+	damage.highFivesGuys();
+
+	// Zero amount tests //
+	std::cout << "\nExecuting zero amount tests (FragTrap):" << std::endl;
+	zero.takeDamage(0);
+	zero.beRepaired(0);
+
+	// Destructors //
+	std::cout << "\nDestructors:" << std::endl;
+}
+
+int main() {
+	scavTests();
+	fragTests();
+	clapTests();
 	return 0;
 }
