2ba6ce8608f1a07b522295c24f8022a34f9a43fe
xDec0de <xdec0deyt@gmail.com>
2025-08-09 16:54:57 +0200

    Update 01/04 so it works on CPP 98 + Optimizations

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -3,52 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:24:32 by danimart          #+#    #+#             */
-/*   Updated: 2024/03/04 20:31:23 by danimart         ###   ########.fr       */
+/*   Updated: 2025/08/09 16:53:12 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <string>
 
 int prog_exit(const std::string &msg, int code) {
 	std::cout << msg << std::endl;
 	exit(code);
 }
 
-int sed(std::ifstream &file, std::string &filename, const std::string &to_find, const std::string &replacement) {
-	std::ofstream copy(filename.append(".replace"), std::ofstream::out);
-	std::string content;
-	std::getline(file, content, '\0');
-	const int tf_len = to_find.length();
-	file.close();
-	if (tf_len == 0)
-		std::cout << "Warning: The text to replace is empty, the content won't be modified." << std::endl;
-	else if (replacement.length() == 0)
-		std::cout << "Warning: The replacement string is empty, the content won't be modified." << std::endl;
-	else if (!to_find.compare(replacement))
-		std::cout << "Warning: The text to replace is equal to the replacement, the content won't be modified." << std::endl;
-	else {
-		int last_index = content.find(to_find, 0);
-		while (last_index != -1) {
-			content.erase(last_index, tf_len);
-			content.insert(last_index, replacement);
-			last_index = content.find(to_find, last_index + 1);
-		}
+std::string replace_all(const std::string &line, const std::string &to_find, const std::string &replacement) {
+	if (to_find.empty() || to_find == replacement || replacement.empty())
+		return line;
+	std::string result;
+	std::string::size_type pos = 0;
+	std::string::size_type found;
+	while ((found = line.find(to_find, pos)) != std::string::npos) {
+		result.append(line, pos, found - pos);
+		result.append(replacement);
+		pos = found + to_find.length();
 	}
-	copy << content;
+	result.append(line, pos, line.length() - pos);
+	return result;
+}
+
+void sed(std::ifstream &file, const std::string &filename, const std::string &to_find, const std::string &replacement) {
+	std::ofstream copy((filename + ".replace").c_str(), std::ofstream::out);
+	std::string line;
+	while (std::getline(file, line))
+		copy << replace_all(line, to_find, replacement) << '\n';
+	file.close();
 	copy.close();
-	return 0;
 }
 
 int main(int argc, char const *argv[]) {
 	if (argc != 4)
 		prog_exit("Usage: ./sed <file name> <text to replace> <replacement>", 1);
 	std::string filename = argv[1];
-	std::ifstream file(filename, std::ifstream::in);
+	std::ifstream file(filename.c_str(), std::ifstream::in);
 	if (!file.is_open())
 		prog_exit("Error: Could not open file \"" + filename + "\", please check if the file exists.", 2);
-	return sed(file, filename, argv[2], argv[3]);
+	sed(file, filename, argv[2], argv[3]);
+	return 0;
 }
