135f964a66af13a63a32f7a74347629b289e2e3a
xDec0de <xdec0deyt@gmail.com>
2025-08-10 20:19:24 +0200

    02/02: Added comparison operators

---
+++ module02/ex02/Fixed.cpp
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/09 20:31:07 by daniema3          #+#    #+#             */
+/*   Updated: 2025/08/10 20:11:54 by daniema3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+/*
+ - Constructors
+ */
+
+Fixed::Fixed(void) : _value(0) {
+	std::cout << "Default consctuctor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed::Fixed(const int intValue) {
+	std::cout << "Int constructor called" << std::endl;
+	this->_value = intValue << _bits;
+}
+
+Fixed::Fixed(const float floatValue) {
+	std::cout << "Float constructor called" << std::endl;
+	this->_value = roundf(floatValue * (1 << _bits));
+}
+
+/*
+ - Destructor
+ */
+
+Fixed::~Fixed(void) {
+	std::cout << "Desctuctor called" << std::endl;
+}
+
+/*
+ - Assignement operator
+ */
+
+Fixed &Fixed::operator = (const Fixed &other) {
+	std::cout << "Copy assignement operator called" << std::endl;
+	this->_value = other._value;
+	return *this;
+}
+
+/*
+ - Comparison operators
+ */
+
+bool Fixed::operator > (const Fixed &other) const {
+	return this->toFloat() > other.toFloat();
+}
+
+bool Fixed::operator < (const Fixed &other) const {
+	return this->toFloat() < other.toFloat();
+}
+
+bool Fixed::operator >= (const Fixed &other) const {
+	return this->toFloat() >= other.toFloat();
+}
+
+bool Fixed::operator <= (const Fixed &other) const {
+	return this->toFloat() <= other.toFloat();
+}
+
+bool Fixed::operator == (const Fixed &other) const {
+	return this->toFloat() == other.toFloat();
+}
+
+bool Fixed::operator != (const Fixed &other) const {
+	return this->toFloat() != other.toFloat();
+}
+
+/*
+ - Insertion operator
+ */
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed) {
+	out << fixed.toFloat();
+	return out;
+}
+
+/*
+ - Raw bits
+ */
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_value;
+}
+
+void Fixed::setRawBits(const int raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+}
+
+/*
+ - Primitive type conversion
+ */
+
+int Fixed::toInt(void) const {
+	return _value >> _bits;
+}
+
+float Fixed::toFloat(void) const {
+	return ((float) _value) / (1 << _bits);
+}
---
+++ module02/ex02/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniema3 <daniema3@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/09 20:31:24 by daniema3          #+#    #+#             */
+/*   Updated: 2025/08/09 21:11:54 by daniema3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main( void ) {
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+
+	a = Fixed( 1234.4321f );
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	return 0;
+}
